656cae88e64d46fcae03ee70b8d8891838eee5f4
nahum-nieves <anahumnieves@gmail.com>
2025-03-31 09:13:01 -0600

    Adding actividad2

---
+++ actividad2/actividad2.py
@@ -0,0 +1,7 @@
+"""
+https://jupyter.org/install
+https://www.kaggle.com/datasets/the-guardian/olympic-games
+https://anaconda.org/anaconda/pandas
+https://tug.org/mactex/
+https://saturncloud.io/blog/how-to-convert-ipynb-to-pdf-in-jupyter-notebook/
+"""
